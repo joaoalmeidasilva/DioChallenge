72420c313ed992dedfd44ae4d60f0f0bbe42da19
joaoalmeidasilva99 <joaoalmeidasilva99@gmail.com>
2024-08-15 21:58:48 -0300

    código corrigido

--- Conversao.go
+++ Conversao.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 
-	const TempK := 373.2
+	const TempK = 373.2
 	var tempC float64 = TempK - 273
 
 	fmt.Printf("O ponto de ebulição da água em Kelvin é: %g e o ponto de ebulição da água em Celsius é %g", TempK, tempC)
