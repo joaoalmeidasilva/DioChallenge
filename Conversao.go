d7b4215049d4f43375c029b6eae24af69024d076
joaoalmeidasilva99 <joaoalmeidasilva99@gmail.com>
2024-08-15 21:58:01 -0300

    Subindo o projeto para o git

--- Conversao.go
+++ Conversao.go
@@ -1,5 +1,12 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	const TempK := 373.2
+	var tempC float64 = TempK - 273
+
+	fmt.Printf("O ponto de ebulição da água em Kelvin é: %g e o ponto de ebulição da água em Celsius é %g", TempK, tempC)
 
 }
